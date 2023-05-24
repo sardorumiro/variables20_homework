9f905c53844c7b1ec4899f1f54baef774db469d7
davlat <name@umirovsardor693@gmail.com>
2023-05-24 14:10:12 +0500

    a

--- bin/variables20_homework02.dart
+++ bin/variables20_homework02.dart
@@ -1,3 +1,6 @@
 //Task 2:
 //Declare a variable named temperature of type double and assign it any decimal value.
-void main() {}
+void main() {
+ double temperature = 1.1;
+ print (temperature);     
+}
