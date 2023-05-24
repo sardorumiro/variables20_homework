926c96a9a978098f8bbd792bec0363326aeaff9b
davlat <name@umirovsardor693@gmail.com>
2023-05-24 14:45:50 +0500

    a

--- bin/variables20_homework11.dart
+++ bin/variables20_homework11.dart
@@ -1,3 +1,6 @@
 //Task 11:
 //Declare a variable named height of type double and assign it any decimal value representing your height.
-void main() {}
+void main() {
+ double height = 70;
+ print(height);   
+}
