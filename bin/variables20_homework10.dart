50f65329604e9a844f70296ec84fee5e4d4e9da2
davlat <name@umirovsardor693@gmail.com>
2023-05-24 14:43:12 +0500

    a

--- bin/variables20_homework10.dart
+++ bin/variables20_homework10.dart
@@ -1,3 +1,6 @@
 //Task 10:
 //Create a variable named isWeekend of type bool and assign it either true or false depending on whether it is currently a weekend or not.
-void main() {}
+void main() {
+ bool isWeekend =false;
+ print(false);   
+}
