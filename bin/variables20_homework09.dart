2db32530eca8d64baa2ad0cfa3cea12d65815cf6
davlat <name@umirovsardor693@gmail.com>
2023-05-24 14:39:56 +0500

    a

--- bin/variables20_homework09.dart
+++ bin/variables20_homework09.dart
@@ -1,3 +1,6 @@
 //Task 9:
 //Declare a variable named favoriteColor of type String and assign it your favorite color.
-void main() {}
+void main() {
+ String favoriteColor ='qora';
+ print (favoriteColor);   
+}
